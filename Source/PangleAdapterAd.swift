296e46304d04de0465b38b1787d90ec8c8ab3dbe
Daniel Barros <dani.barros@me.com>
2024-04-16 08:40:05 +0530

    PartnerAdapter API 5.0 changes (#39)

--- Source/PangleAdapterAd.swift
+++ Source/PangleAdapterAd.swift
@@ -14,6 +14,9 @@ class PangleAdapterAd: NSObject {
     /// The partner adapter that created this ad.
     let adapter: PartnerAdapter
     
+    /// Extra ad information provided by the partner.
+    var details: PartnerDetails = [:]
+
     /// The ad load request associated to the ad.
     /// It should be the one provided on `PartnerAdapter.makeAd(request:delegate:)`.
     let request: PartnerAdLoadRequest
@@ -23,7 +26,7 @@ class PangleAdapterAd: NSObject {
     weak var delegate: PartnerAdDelegate?
         
     /// The completion handler to notify Chartboost Mediation of ad show completion result.
-    var showCompletion: ((Result<PartnerEventDetails, Error>) -> Void)?
+    var showCompletion: ((Result<PartnerDetails, Error>) -> Void)?
     
     /// Create a new instance of the adapter.
     /// - Parameters:
