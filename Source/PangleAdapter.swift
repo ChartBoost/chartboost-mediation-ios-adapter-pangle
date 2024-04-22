918d60b5ace8caef6d75cebc75b7e48957242fa9
Daniel Barros <daniel.barros@chartboost.com>
2024-04-22 10:35:46 -0700

    Remove unnecessary return keywords

--- Source/PangleAdapter.swift
+++ Source/PangleAdapter.swift
@@ -116,7 +116,7 @@ final class PangleAdapter: PartnerAdapter {
     /// - parameter delegate: The delegate that will receive ad life-cycle notifications.
     func makeBannerAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerBannerAd {
         // This partner supports multiple loads for the same partner placement.
-        return PangleAdapterBannerAd(adapter: self, request: request, delegate: delegate)
+        PangleAdapterBannerAd(adapter: self, request: request, delegate: delegate)
     }
 
     /// Creates a new ad object in charge of communicating with a single partner SDK ad instance.
