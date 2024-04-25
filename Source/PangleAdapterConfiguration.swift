9ebc947efba893e371ffc5ecaea06ab7d651cebe
Daniel Barros <daniel.barros@chartboost.com>
2024-04-25 13:41:20 -0700

    Add public keyword

--- Source/PangleAdapterConfiguration.swift
+++ Source/PangleAdapterConfiguration.swift
@@ -9,18 +9,18 @@ import PAGAdSDK
 @objc public class PangleAdapterConfiguration: NSObject {
 
     /// The version of the partner SDK.
-    @objc static var partnerSDKVersion: String {
+    @objc public static var partnerSDKVersion: String {
         PAGSdk.sdkVersion
     }
 
     /// The version of the adapter.
     /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
     /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    @objc static let adapterVersion = "4.5.7.0.0.0"
+    @objc public static let adapterVersion = "4.5.7.0.0.0"
 
     /// The partner's unique identifier.
-    @objc static let partnerID = "pangle"
+    @objc public static let partnerID = "pangle"
 
     /// The human-friendly partner name.
-    @objc static let partnerDisplayName = "Pangle"
+    @objc public static let partnerDisplayName = "Pangle"
 }
